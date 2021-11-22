d7105f70a5bbb518cf3b12ed84c53799fd406a28
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-11-22 16:16:23 +0100

    avance

---
+++ operagx.ps1
@@ -0,0 +1,10 @@
+$timeout = new-timespan -Minutes 720
+$sw = [diagnostics.stopwatch]::StartNew()
+while ($sw.elapsed -lt $timeout){
+	[void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
+	[Microsoft.VisualBasic.Interaction]::AppActivate("opera") 
+	[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms") 
+	[System.Windows.Forms.SendKeys]::SendWait("Coucou")
+    start-sleep -seconds 10
+}
+write-host "1FORMATIK.com SendWait"
